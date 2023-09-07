39dfab9a6536623e17ec7396c2b4b1b9be5bbf80
feruzka1706@gmail.com <feruzka1706@gmail.com>
2023-09-06 22:46:08 -0500

    new code examples has been added

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -1,9 +1,17 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import static java.util.Arrays.*;
 
 @Controller
 @RequestMapping
@@ -13,6 +21,20 @@ public class StudentController {
     public String homePage(Model model){
         model.addAttribute("name","Cydeo");
         model.addAttribute("course","MVC");
+
+        String subject = "Collections";
+        model.addAttribute("subject",subject);
+
+        //create some random studentId (0-1000) and show it in your UI
+        int studentId = new Random().nextInt(1000);
+        model.addAttribute("studentId",studentId);
+
+        List<Integer> numbers = Arrays.asList(4,5,7,10);
+        model.addAttribute("numbers",numbers);
+
+        Student student = new Student(1,"Mike","Smith");
+        model.addAttribute("student",student);
+
         return "student/welcome";
     }
 
