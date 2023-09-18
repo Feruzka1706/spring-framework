3f5f74230786b7d0968671026cef060ca922b2da
feruzka1706@gmail.com <feruzka1706@gmail.com>
2023-09-17 20:36:35 -0500

    Added Spring-09-Thymeleaf module

---
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/model/Student.java
@@ -0,0 +1,19 @@
+package com.cydeo.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String state;
+
+
+}
