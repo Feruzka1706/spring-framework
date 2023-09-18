2ef52346367d60cf5f46f145c51de75da74d1854
feruzka1706@gmail.com <feruzka1706@gmail.com>
2023-09-17 21:52:45 -0500

    Thymeleaf code changes

--- Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -1,17 +1,31 @@
 package com.cydeo.controller;
 
 
+import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
     @GetMapping("/register")
-    public String studentRegister(){
+    public String studentRegister(Model model){
+
+        model.addAttribute("students", DataGenerator.createStudents());
 
         return "student/register";
+    }
+
+    @GetMapping("/welcome")
+    public String studentWelcome(@RequestParam("id") String student, Model model){
+
+        //Displaying request parameter data to the browser
+        model.addAttribute("id",student);
+        model.addAttribute("students", DataGenerator.createStudents());
+        return "student/welcome";
     }
 }
