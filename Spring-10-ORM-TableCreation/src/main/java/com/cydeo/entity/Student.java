f8c272f622cc93f2cf9a072d971eac02873464c3
feruzka1706@gmail.com <feruzka1706@gmail.com>
2023-10-20 22:00:34 -0500

    Adding DB connection part

--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -1,23 +1,25 @@
 package com.cydeo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "student")
 public class Student {
 
-
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     //if field names are camelCase, spring will generate by default column names
     //as following format: first_name
+    //or we can specify column names explicitly
+    @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "email")
     private String email;
 
 }
