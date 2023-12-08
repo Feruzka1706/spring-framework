31192e3c88cdc33bbdf825b1e2cf5519bc1446f5
feruzka1706@gmail.com <feruzka1706@gmail.com>
2023-12-07 22:02:56 -0600

    Started API code changes

--- Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -7,11 +7,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController  //@Controller + @ResponseBody
-@RequestMapping("/courses")
+@RequestMapping("/courses/api/v1")
 public class CourseController {
 
     private final CourseService courseService;
-
 
     public CourseController(CourseService courseService) {
         this.courseService = courseService;
@@ -31,6 +30,35 @@ public class CourseController {
         return courseService.getCourseById(courseId);
     }
 
+
+
+    @GetMapping("category/{name}")
+    public List<CourseDTO> getCourseByCategory(@PathVariable("name") String category){
+
+        return courseService.getCoursesByCategory(category);
+    }
+
+
+    @PostMapping
+    public CourseDTO createCourse(@RequestBody CourseDTO courseDTO){
+        return courseService.createCourse(courseDTO);
+    }
+
+
+    @PutMapping("{id}")
+    public void updateCourse(@PathVariable("id") Long courseId, @RequestBody CourseDTO courseDTO){
+         courseService.updateCourse(courseId,courseDTO);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteCourse(@PathVariable("id") Long courseId){
+        courseService.deleteCourseById(courseId);
+    }
+
+    @DeleteMapping
+    public void deleteCourses(){
+        courseService.deleteCourses();
+    }
 
 
 }
--- Spring-15-REST/src/main/java/com/cydeo/entity/ResponseWrapper.java
+++ Spring-15-REST/src/main/java/com/cydeo/entity/ResponseWrapper.java
@@ -5,18 +5,25 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class ResponseWrapper {
 
+    /**
+     * This class is for serialization and deserialization flow
+     */
+
     private boolean success;
     private String message;
     private Integer code;
     private Object data;
 
-    public ResponseWrapper(String message, Object data) {
+    public ResponseWrapper( String message, Object data) {
         this.success = true;
         this.message = message;
         this.code = HttpStatus.OK.value();
@@ -28,4 +35,5 @@ public class ResponseWrapper {
         this.code = HttpStatus.OK.value();
         this.success = true;
     }
+
 }
