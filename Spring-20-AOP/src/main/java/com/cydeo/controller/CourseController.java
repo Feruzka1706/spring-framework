d53c9f3480e6f3d4f72b6da0fefcf0ab0ae29d2e
feruzka1706@gmail.com <feruzka1706@gmail.com>
2023-12-24 21:24:37 -0600

    Spring - REST 20 - AOP Aspect Oriented Programming modules

--- Spring-20-AOP/src/main/java/com/cydeo/controller/CourseController.java
+++ Spring-20-AOP/src/main/java/com/cydeo/controller/CourseController.java
@@ -1,10 +1,8 @@
 package com.cydeo.controller;
 
-import com.cydeo.annotation.Loggable;
 import com.cydeo.dto.CourseDTO;
 import com.cydeo.service.CourseService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,7 +28,7 @@ public class CourseController {
         return list;
     }
 
-    @Loggable
+
     @GetMapping("/{id}")
     public CourseDTO getCourseById(@PathVariable("id") Long courseId) {
         return courseService.getCourseById(courseId);
@@ -41,7 +39,7 @@ public class CourseController {
         return courseService.getCoursesByCategory(category)       ;
     }
 
-    @Loggable
+
     @PostMapping
     public CourseDTO createCourse(@RequestBody CourseDTO course) {
         return courseService.createCourse(course);
